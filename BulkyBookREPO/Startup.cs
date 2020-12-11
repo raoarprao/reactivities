d84839eb48adc656ba115422b3c6d821a7d4e8bb
raoadarsh@gmail.com <54360608+raoadarsh@users.noreply.github.com>
2020-12-11 23:19:58 +0530

    Revert "Added data utility and model library"
    This reverts commit 3d535b3586810d6a505b68bb876ee9adcf133289.

--- BulkyBookREPO/Startup.cs
+++ BulkyBookREPO/Startup.cs
@@ -8,10 +8,10 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
+using BulkyBookREPO.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BulkyBookREPO.Data;
 
 namespace BulkyBookREPO
 {
@@ -30,9 +30,9 @@ namespace BulkyBookREPO
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
@@ -62,7 +62,7 @@ namespace BulkyBookREPO
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
